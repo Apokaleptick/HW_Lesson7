b74093ad24e6d6611a545c0f392cc197bcb3866d
Serghei Sipov <serfen95@gmail.com>
2024-07-29 09:43:50 +0300

    Добавил решение Задания2

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿void Main()
+{
+
+    int m = 3;
+    int n = 7;
+
+    Console.WriteLine($"Функция Аккермана A({m}, {n}) = {Acker(m, n)}");
+}
+
+int Acker(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Acker(m - 1, 1);
+    }
+    else
+    {
+        return Acker(m - 1, Acker(m, n - 1));
+    }
+}
+Main();
