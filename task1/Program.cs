ee6666afe4854783e26ef1f115d62b096714d132
Serghei Sipov <serfen95@gmail.com>
2024-07-29 09:43:31 +0300

    Добавил решение Задания1

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿void Main()
+{
+    int M = 5;
+    int N = 10;
+
+    Console.WriteLine($"Натуральные числа в промежутке от {M} до {N}: ");
+    PrintNumbersInRange(M, N);
+}
+
+static void PrintNumbersInRange(int M, int N)
+{
+
+    if (M > N)
+    {
+        return;
+    }
+
+    Console.Write(M + " ");
+    PrintNumbersInRange(M + 1, N);
+}
+Main();
+
