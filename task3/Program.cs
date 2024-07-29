10124bac1e25faa0df6ec829b95c71c744f68778
Serghei Sipov <serfen95@gmail.com>
2024-07-29 11:15:46 +0300

    Добавил изменение в Задание3

--- task3/Program.cs
+++ task3/Program.cs
@@ -1,20 +1,46 @@
 ﻿void Main()
 {
-    int[] array = { 1, 2, 3, 4, 5 };
+    Console.Write("Введите количество элементов массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("Элементы массива в обратном порядке:");
-    PrintArrayReverse(array, array.Length - 1);
+    int[] array = new int[length];
+    Random random = new Random();
+
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(1, 101);
+    }
+    System.Console.Write("Начальный массив:");
+    PrintArray(array, 0);
+    System.Console.WriteLine();
+
+    Console.Write("Элементы массива в обратном порядке:");
+    PrintArrayInReverse(array, length - 1);
+    System.Console.WriteLine();
 }
 
-void PrintArrayReverse(int[] array, int index)
+void PrintArray(int[] array, int index)
+{
+    if (index >= array.Length)
+        return;
+
+    Console.Write(array[index]);
+    if (index < array.Length - 1)
+    {
+        Console.Write(", ");
+    }
+    PrintArray(array, index + 1);
+}
+void PrintArrayInReverse(int[] array, int index)
 {
     if (index < 0)
-    {
         return;
+
+    Console.Write(array[index]);
+    if (index > 0)
+    {
+        System.Console.Write(", ");
     }
-
-    Console.Write(array[index] + " ");
-
-    PrintArrayReverse(array, index - 1);
+    PrintArrayInReverse(array, index - 1);
 }
 Main();
